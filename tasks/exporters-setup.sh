56bdb490afa81de4383076dccd01212214410c83
GeethaPandiyan <52655312+GeethaPandiyan@users.noreply.github.com>
2019-08-06 01:03:14 +0530

    Delete exporters-setup.sh

--- tasks/exporters-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f mongoext-deployment.yaml,mongoext-svc.yaml,mongoext-sm.yaml -n monitoring
-kubectl apply -f cadvisor.yaml,cadvisor-svc.yaml,cadvisor-sm.yaml -n monitoring
